12ebc89c2148e5f28ab2d26906face2a81df0637
LMK <luongminhkhang@gmail.com>
2019-08-12 07:22:16 +0700

    refactor code

--- lib/chat_app_web/channels/room_channel.ex
+++ lib/chat_app_web/channels/room_channel.ex
@@ -1,13 +1,13 @@
 defmodule ChatAppWeb.RoomChannel do
 	use ChatAppWeb, :channel
 
-	def join("room" <> room_id, _params, socket) do
-		{:ok, %{channel: "room#{room_id}"}, assign(socket, :room_id, room_id)}
+	def join("room:" <> room_id, _params, socket) do
+		{:ok, %{channel: "room:#{room_id}"}, assign(socket, :room_id, room_id)}
 	end
 
 	def handle_in("message:add", %{"message" => body}, socket) do
 		room_id = socket.assigns[:room_id]
-		broadcast!(socket, "room#{room_id}:new_message", %{body: body})
+		broadcast!(socket, "room:#{room_id}:new_message", %{body: body})
 		{:reply, :ok, socket}
 	end
 end
